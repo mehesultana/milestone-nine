9d5c8f59c52df5d46501550b2b97a11bc1877732
mehesultana <sultanamehe01@gmail.com>
2021-09-29 01:30:56 +0600

    Navigate Pages, set header, Create Link, display active route

--- 52-simple-react-router-examples/simple-react-router/src/App.js
+++ 52-simple-react-router-examples/simple-react-router/src/App.js
@@ -1,13 +1,32 @@
+import { Route, Switch } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import About from './components/About/About';
 import Friends from './components/Friends/Friends';
+import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import NotFound from './components/NotFound/NotFound';
 
 function App() {
 	return (
 		<div className="App">
-			<Friends></Friends>
+			<BrowserRouter>
+				<Header></Header>
+				<Switch>
+					<Route path="/home">
+						<Home></Home>
+					</Route>
+					<Route path="/friends">
+						<Friends></Friends>
+					</Route>
+					<Route path="/about">
+						<About></About>
+					</Route>
+					<Route path="*">
+						<NotFound></NotFound>
+					</Route>
+				</Switch>
+			</BrowserRouter>
 		</div>
 	);
 }
---
+++ 52-simple-react-router-examples/simple-react-router/src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+	const activeStyle = { fontWeight: 'bold', color: 'red' };
+	return (
+		<nav>
+			<NavLink activeStyle={activeStyle} to="/home">
+				home
+			</NavLink>
+			<NavLink activeStyle={activeStyle} to="/friends">
+				friends
+			</NavLink>
+			<NavLink activeStyle={activeStyle} to="/about">
+				about
+			</NavLink>
+		</nav>
+	);
+};
+
+export default Header;
