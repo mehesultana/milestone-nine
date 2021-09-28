6066be75988de915c2b92015a76057e3ae159047
mehesultana <sultanamehe01@gmail.com>
2021-09-28 17:14:06 +0600

    Setup tailwind css with Create React App

---
+++ 51-react-bootstrap-material-ui-axios-rechart/react-and-tailwind/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+	return (
+		<div className="App">
+			<p className="bg-yellow-400 text-4xl hover:text-blue-400">this is Mehe</p>
+			<p className="bg-yellow-200">this is Mehe</p>
+			<p className="text-primary">hellooooooo</p>
+			<p className="text-custom">my Custom</p>
+		</div>
+	);
+}
+
+export default App;
