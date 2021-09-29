2a18f9c0a8715bb1c08d02aa56e1eb8c54ec5f77
mehesultana <sultanamehe01@gmail.com>
2021-09-30 03:03:05 +0600

    components, input field, styles

--- 52.5-meal-db-search-and-routing/the-meal-db/src/components/Header/Header.js
+++ 52.5-meal-db-search-and-routing/the-meal-db/src/components/Header/Header.js
@@ -4,14 +4,21 @@ import { NavLink } from 'react-router-dom';
 import logo from '../../images/logo.png';
 
 const Header = () => {
+	const style = { fontWeight: 'bold', color: 'white', marginRight: 100, marginLeft: 100 };
 	return (
 		<Navbar bg="dark" variant="dark">
 			<img className="logo" src={logo} alt="" />
 			<Container>
 				<Nav className="me-5">
-					<NavLink to="/home">Home</NavLink>
-					<NavLink to="/home">Meal</NavLink>
-					<NavLink to="/home">Home</NavLink>
+					<NavLink activeStyle={style} to="/home">
+						Home
+					</NavLink>
+					<NavLink activeStyle={style} to="/home">
+						Meal
+					</NavLink>
+					<NavLink activeStyle={style} to="/home">
+						Home
+					</NavLink>
 				</Nav>
 			</Container>
 		</Navbar>
--- 52.5-meal-db-search-and-routing/the-meal-db/src/components/Home/Home.js
+++ 52.5-meal-db-search-and-routing/the-meal-db/src/components/Home/Home.js
@@ -1,17 +1,33 @@
+import Button from '@restart/ui/esm/Button';
 import React, { useEffect, useState } from 'react';
-import { Container, Row } from 'react-bootstrap';
+import { Container, FormControl, InputGroup, Row } from 'react-bootstrap';
 import Meal from '../Meal/Meal';
+import './Home.css';
 
 const Home = () => {
 	const [meals, setMeals] = useState([]);
-	const url = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+	const [searchText, setSearchText] = useState('');
+	const handleSearchField = (e) => {
+		const searchTextValue = e.target.value;
+		setSearchText(searchTextValue);
+	};
+
+	const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
 	useEffect(() => {
 		fetch(url)
 			.then((res) => res.json())
 			.then((data) => setMeals(data.meals));
-	}, []);
+	}, [searchText]);
 	return (
 		<Container>
+			<h1>Welcome to TheMealDB</h1>
+			<InputGroup className="mb-5 mt-5">
+				<FormControl onChange={handleSearchField} placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2" />
+				<Button variant="outline-secondary" id="button-addon2">
+					Button
+				</Button>
+			</InputGroup>
+			{/* <input onChange={handleSearchField} placeholder="search your favourite meal" type="text" /> */}
 			<Row xs={1} md={4} className="g-4">
 				{meals.map((meal) => (
 					<Meal meal={meal}></Meal>
