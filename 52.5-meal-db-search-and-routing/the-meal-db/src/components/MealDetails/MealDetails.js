04020031ee08450c332ac85d000dd22416c270e8
mehesultana <sultanamehe01@gmail.com>
2021-09-30 04:09:51 +0600

    display details

--- 52.5-meal-db-search-and-routing/the-meal-db/src/components/MealDetails/MealDetails.js
+++ 52.5-meal-db-search-and-routing/the-meal-db/src/components/MealDetails/MealDetails.js
@@ -9,12 +9,16 @@ const MealDetails = () => {
 		const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${strMeal}`;
 		fetch(url)
 			.then((res) => res.json())
-			.then((data) => setMeal(data));
-	}, []);
+			.then((data) => setMeal(data.meals[0]));
+	}, [strMeal]);
+	const { strCategory, strArea, strInstructions, strMealThumb } = meal;
 	return (
 		<div>
+			<img src={strMealThumb} alt="" />
 			<h1>Name: {strMeal}</h1>
-			<h2>Category: {meal.strCategory}</h2>
+			<h2>Category: {strCategory}</h2>
+			<h2>Area: {strArea}</h2>
+			<p>Instructions: {strInstructions}</p>
 		</div>
 	);
 };
