2adc5a288f310bf7da0f794fe12dc21420b414b7
mehesultana <sultanamehe01@gmail.com>
2021-10-01 02:28:10 +0600

    create useCart and use it

--- 53-ema-john-with-router-and-custom-hook/ema-john-with-router2/src/components/OrderReview/OrderReview.js
+++ 53-ema-john-with-router-and-custom-hook/ema-john-with-router2/src/components/OrderReview/OrderReview.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import useProducts from '../../hooks/useProducts';
+import useCart from '../../hooks/useCart';
 
 const OrderReview = () => {
-	const [products] = useProducts();
+	const [products, setProducts] = useProducts();
+	const [cart] = useCart(products);
 	return (
 		<div>
 			<h1>{products.length}</h1>
+			<h3>{cart.length}</h3>
 			<h2>This is Order Review</h2>
 		</div>
 	);
