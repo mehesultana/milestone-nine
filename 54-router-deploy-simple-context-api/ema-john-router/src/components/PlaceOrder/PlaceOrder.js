db2ac535f0bbb73b86ce5a2343b7ea32d52f2322
mehesultana <sultanamehe01@gmail.com>
2021-10-02 02:09:37 +0600

    update quantity of existing item

--- 54-router-deploy-simple-context-api/ema-john-router/src/components/PlaceOrder/PlaceOrder.js
+++ 54-router-deploy-simple-context-api/ema-john-router/src/components/PlaceOrder/PlaceOrder.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import img from '../../images/giphy.gif';
+import '../../App.css';
 
 const PlaceOrder = () => {
 	return (
-		<div>
+		<div className="App">
 			<img src={img} alt="" />
 		</div>
 	);
