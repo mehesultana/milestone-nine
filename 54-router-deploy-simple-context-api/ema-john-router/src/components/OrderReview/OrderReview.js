2dc5c9dae91237efa2b9491e32e79e1d204d0db2
mehesultana <sultanamehe01@gmail.com>
2021-10-02 01:45:35 +0600

    Create order placed and clear cart and stored data

--- 54-router-deploy-simple-context-api/ema-john-router/src/components/OrderReview/OrderReview.js
+++ 54-router-deploy-simple-context-api/ema-john-router/src/components/OrderReview/OrderReview.js
@@ -3,11 +3,13 @@ import useProducts from '../../hooks/useProducts';
 import useCart from '../../hooks/useCart';
 import Cart from '../Cart/Cart';
 import ReviewItem from '../ReviewItem/ReviewItem';
-import { removeFromDb } from '../../utilities/fakedb';
+import { clearTheCart, removeFromDb } from '../../utilities/fakedb';
+import { useHistory } from 'react-router';
 
 const OrderReview = () => {
 	const [products, setProducts] = useProducts();
 	const [cart, setCart] = useCart(products);
+	const history = useHistory();
 
 	const handleRemove = (key) => {
 		//console.log(key);
@@ -15,6 +17,13 @@ const OrderReview = () => {
 		setCart(newCart);
 		removeFromDb(key);
 	};
+
+	const handlePlaceOrder = () => {
+		history.push('/placeorder');
+		setCart([]);
+		clearTheCart();
+	};
+
 	return (
 		<div className="shop-container">
 			<div className="product-container">
@@ -24,7 +33,9 @@ const OrderReview = () => {
 			</div>
 			<div className="cart-container">
 				<Cart cart={cart}>
-					<button className="btn-regular">Place Order</button>
+					<button onClick={handlePlaceOrder} className="btn-regular">
+						Place Order
+					</button>
 				</Cart>
 			</div>
 		</div>
